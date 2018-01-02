0ddf8a59dd174a150d7df88e2c081f425bc04800
Clement Udensi <cklemzey@gmail.com>
2018-01-02 11:23:49 +0100

    fixed history-api-fallback for the node server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 const express = require('express');
 const app = express();
+const history = require('connect-history-api-fallback');
 
 
 const port = process.env.PORT || 8000;
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use(history());
 app.use(express.static("./public"));
 app.listen(port, function(){
     console.log('App is running on port 8000');
